b1361736f4575f231d971b0526d00246de1be781
sdarwin <samuel.d.darwin@gmail.com>
2020-01-09 21:08:20 +0000

    vnc_bashrc_startup

--- scripts/qa.sh
+++ scripts/qa.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# 
-# Install VNC locally, with default settings.
+#
+# Installs most prerequisites for testing, without running the final "ansible-playbook" command.
 #
 # Instructions:
 #
-# curl -sL https://raw.githubusercontent.com/sdarwin/Ansible-VNC/master/scripts/quickinstall.sh | sudo bash
+# curl -sL https://raw.githubusercontent.com/sdarwin/Ansible-VNC/master/scripts/qa.sh | sudo bash
 #
 #
 
@@ -38,11 +38,13 @@ then
   fi
   if [[ -f /bin/yum ]]
   then
-    sudo yum install -y epel-release || true
+    sudo yum install -y epel-release
     sudo yum install -y python3-pip
     sudo pip3 install ansible
   fi
 fi
 
 sudo mkdir -p /etc/ansible/roles
-sudo env PATH=$PATH ansible-pull -U https://github.com/sdarwin/Ansible-VNC -d /etc/ansible/roles/sdarwin.VNC -i scripts/localinventory default.yml
+cd /etc/ansible/roles
+sudo git clone https://github.com/sdarwin/Ansible-VNC sdarwin.VNC
+
