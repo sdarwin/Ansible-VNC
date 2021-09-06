b3bf733313bb707608cfd050e6bae36bf222a9e5
Michael De La Rue <tiggmal.mddlr@9ox.net>
2021-09-06 10:22:27 +0100

    Linting fixes

--- scripts/qa.sh
+++ scripts/qa.sh
@@ -29,7 +29,7 @@ then
   fi
 fi
 
-if ! which ansible 
+if ! which ansible
 then
   if [[ -f /usr/bin/apt-get ]]
   then
@@ -48,4 +48,3 @@ fi
 sudo mkdir -p /etc/ansible/roles
 cd /etc/ansible/roles
 sudo git clone https://github.com/sdarwin/Ansible-VNC sdarwin.vnc
-
