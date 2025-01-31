8aa5a6399ff53fd9bb840adad759778846d7b0d3
sdarwin <samuel.d.darwin@gmail.com>
2025-01-31 22:12:51 +0000

    Refresh quickinstall scripts

--- scripts/qa.sh
+++ scripts/qa.sh
@@ -10,8 +10,7 @@
 # cd /etc/ansible
 # ansible-playbook --extra-vars "vnc_desktop=xfce4" roles/sdarwin.vnc/default.yml
 
-set -x
-set -e
+set -xe
 PATH=/usr/local/bin:$PATH
 
 if [[ -f /usr/bin/apt-get ]]
@@ -19,32 +18,29 @@ then
     sudo apt-get update
 fi
 
-if ! which git
-then
-  if [[ -f /usr/bin/apt-get ]]
-  then
-    sudo apt-get install -y git
-  fi
-  if [[ -f /bin/yum ]]
-  then
-    sudo yum install -y git
-  fi
+if ! which git; then
+    if [[ -f /usr/bin/apt-get ]]; then
+        sudo apt-get install -y git
+    fi
+    if [[ -f /bin/yum ]]; then
+        sudo yum install -y git
+    fi
 fi
 
-if ! which ansible
-then
-  if [[ -f /usr/bin/apt-get ]]
-  then
-    sudo apt-get install -y python3-pip
-    sudo pip3 install ansible ansible-lint yamllint
-  fi
-  if [[ -f /bin/yum ]]
-  then
-    . /etc/os-release
-    sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-${VERSION_ID:0:1}.noarch.rpm || true
-    sudo yum install -y python3-pip
-    sudo pip3 install ansible
-  fi
+if ! which ansible; then
+    if [[ -f /usr/bin/apt-get ]]; then
+        sudo apt-get install -y python3-pip python3-venv
+    fi
+    if [[ -f /bin/yum ]]; then
+        . /etc/os-release
+        sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-${VERSION_ID:0:1}.noarch.rpm || true
+        sudo yum install -y python3-pip python3-venv
+    fi
+    if [ ! -d ~/venv ]; then
+        python3 -m venv ~/venv
+    fi
+    . ~/venv/bin/activate
+    pip3 install ansible ansible-lint yamllint
 fi
 
 sudo mkdir -p /etc/ansible/roles
